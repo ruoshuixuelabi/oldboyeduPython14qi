8baaf5151815d9ec92db0562c2346c9d43cb378b
ruoshui <524991368@qq.com>
2020-01-31 18:41:08 +0800

    添加readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# oldboyeduPython14qi
+
+b站上传视频https://www.bilibili.com/video/av85712633/的配套课件和源码
--- 第2部分-面向对象,模块和包(太白-女神)/day16(面向对象-类-对象)/day16-课堂笔记/03 面向对象.py
+++ 第2部分-面向对象,模块和包(太白-女神)/day16(面向对象-类-对象)/day16-课堂笔记/03 面向对象.py
@@ -17,7 +17,6 @@
 #     return count
 
 
-
 # 文件:
 # 1,'alex','未知',1356666666,IT
 
@@ -112,33 +111,32 @@
 # #1,只要类名+() 产生一个对象,自动执行类中的__init__方法.
 
 
-
 class Person:
     '''类体:两部分:变量部分,方法(函数)部分'''
     mind = '有思想'  # 变量,静态变量,静态字段
     animal = '高级动物'
     faith = '有信仰'
 
-    def __init__(self,name,age,hobby):
-        self.name = name  #  Person.money = '运用货币'
+    def __init__(self, name, age, hobby):
+        self.name = name  # Person.money = '运用货币'
         self.age = age
         self.hobby = hobby
 
     def work(self):  # 方法,函数,动态变量
+        print('%s都会工作...' % self.name)
 
-        print('%s都会工作...' %self.name)
     def shop(self):
-
         print('人类可以消费....')
 
+
 # 类名的角度
-    # 操作类中的静态变量
-        # 1, Person.__dict__ 查询类中的所有的内容 (不能进行增删改操作)
+# 操作类中的静态变量
+# 1, Person.__dict__ 查询类中的所有的内容 (不能进行增删改操作)
 # print(Person.__dict__)
 # print(Person.__dict__['faith'])
 # Person.__dict__['mind'] = '无脑'
 # print(Person.__dict__['mind'])
-        # 2, 万能的  .  对类中的单个的变量进行增删改查,用 万能的 点
+# 2, 万能的  .  对类中的单个的变量进行增删改查,用 万能的 点
 # print(Person.mind)
 # print(Person.animal)  # 查
 # Person.money = '运用货币'  # 增
@@ -146,38 +144,37 @@ class Person:
 # del Person.mind
 # print(Person.__dict__)
 
-    #操作类中的方法  (工作中基本不用类名去操作)
+# 操作类中的方法  (工作中基本不用类名去操作)
 # Person.work(111)
 
 # 对象的角度
 # ret = Person('alex',1000, 'oldwomen')  # 类名+()的这个过程:实例化的过程(创建一个对象的过程),
-          # Person() 实例化对象,实例,对象.
+# Person() 实例化对象,实例,对象.
 # print(ret)
 
 
-#1, 类名+()产生一个实例(对象,对象空间.)
-#2, 自动执行类中的__init__方法,将对象空间传给__init__的self参数,
-#3, 给对象封装相应的属性.
+# 1, 类名+()产生一个实例(对象,对象空间.)
+# 2, 自动执行类中的__init__方法,将对象空间传给__init__的self参数,
+# 3, 给对象封装相应的属性.
 # print(ret.__dict__)
 
 # 对象的角度
-    # 操作对象中的静态变量
-        #1, __dict__ 查询对象中的所有的内容
-        #2, 万能的.  万能的 点.
+# 操作对象中的静态变量
+# 1, __dict__ 查询对象中的所有的内容
+# 2, 万能的.  万能的 点.
 # print(ret.name)  # 查
 # ret.high = 175  # 增
 # del ret.name  # 删
 # ret.age = 73 # 改
 # print(ret.__dict__)
-    # 对象操作类中的静态变量 : 只能查询.
+# 对象操作类中的静态变量 : 只能查询.
 # print(ret.mind)
 
-    # 对象调用类中的方法  (工作中 通过对象执行类中的方法,而不是通过类名)
+# 对象调用类中的方法  (工作中 通过对象执行类中的方法,而不是通过类名)
 # ret.shop()
 # print(ret)
 
-bigsum = Person('大阳哥',39, '非男')
-india_ning = Person('印度阿宁',19, '女')
+bigsum = Person('大阳哥', 39, '非男')
+india_ning = Person('印度阿宁', 19, '女')
 bigsum.work()
 india_ning.work()
-
